a27308e52a3f8cfa1aab10cadc123cf9591db63a
domi802 <64065747+domi802@users.noreply.github.com>
2022-04-26 17:40:32 +0200

    Initial Commit

---
+++ MovieSearchApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieSearchApp
+//
+//  Created by Dominik Woźniak on 26/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieSearchApp/MovieSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchAppApp.swift
+//  MovieSearchApp
+//
+//  Created by Dominik Woźniak on 26/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
